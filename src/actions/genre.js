c6cc8ea1f21acbf6f922a28ef1535b577f76cc92
Kemal Yenilmez <kemal.yenilmez@piklook.com>
2016-10-14 09:09:24 +0300

    Pagination completed

--- src/actions/genre.js
+++ src/actions/genre.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const GENRE_MOVIES = 'GENRE_MOVIES';
 
-export function getGenreMovies(genre_id = 1){
-    const request = axios.get(`http://api.sinemateknik.com/v1/genres/${genre_id}/movies`);
+export function getGenreMovies(genre_id = 1, page=1){
+    const request = axios.get(`http://api.sinemateknik.com/v1/genres/${genre_id}/movies?page=${page}`);
     return {
       type: GENRE_MOVIES,
       payload: request
--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getGenreMovies } from '../actions/genre';
 import { Pagination } from 'react-bootstrap';
-
+import { push } from 'react-router-redux';
 
 class MoviesPage extends Component {
+   constructor(props)
+    {
+        super(props);
+        this.changePage = this.changePage.bind(this);
+    }
+
+    changePage(page)
+    {
+        console.log("Page", page);
+        this.props.dispatch(getGenreMovies(1, page));
+    }
 
     componentWillMount(){
         this.props.dispatch(getGenreMovies());
@@ -16,31 +27,23 @@ class MoviesPage extends Component {
         }
 
         return(
-                    this.props.movies.map((movie, index) => {
-                        return (
-                            <div key={index}>{movie.title}</div>
-                         );
+            this.props.movies.map((movie, index) => {
+                return (
+                    <div key={index}>{movie.title}</div>
+                );
                     })
             )
     }
 
     render() {
-    /*
-        const current_page = this.props.meta["pagination"];
-        const per_page = 10;
-        const pages = Math.ceil(this.props.users.length / per_page);
-        const current_page = this.props.page;
-        const start_offset = (current_page - 1) * per_page;
-        let start_count = 0;
-    */
+
         if(!this.props.movies){
            return <div>Loading ...</div>
         }
+
         const pagination = this.props.meta["pagination"];
         const current_page = pagination.current_page;
         const pages = pagination.total_pages;
-
-        console.log("Current Page", current_page);
 
         return (
           <div>
@@ -48,9 +51,11 @@ class MoviesPage extends Component {
                 <h3>Hello Movies!</h3>
                 {this.listMovies()}
 
-<Pagination bsClass="pagination" className="users-pagination pull-right" bsSize="medium" maxButtons={10} first last next prev
-                        boundaryLinks items={pages} activePage={current_page} />
-               
+                <Pagination className="pagination" bsSize="medium" maxButtons={10} first last next prev boundaryLinks
+                items={pages}
+                activePage={current_page}
+                onSelect={this.changePage}/>
+
             </div>
           </div>
         );
@@ -59,6 +64,7 @@ class MoviesPage extends Component {
 
 function mapStateToProps(state) {
     const { data, meta } = state.genre.movies;
+
     return { movies: data, meta: meta };
  }
 
